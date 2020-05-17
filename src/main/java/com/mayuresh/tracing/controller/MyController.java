638b97ce270bc3e9d72885cbb833fd0eb6083313
mayuresh1612 <mayuresh.paranjape2010@gmail.com>
2020-05-17 20:04:32 +0530

    Request Logs added

--- src/main/java/com/mayuresh/tracing/controller/MyController.java
+++ src/main/java/com/mayuresh/tracing/controller/MyController.java
@@ -4,6 +4,7 @@ import com.mayuresh.tracing.config.AppConfig;
 import com.mayuresh.tracing.model.Profile;
 import com.mayuresh.tracing.service.ProfileService;
 import lombok.extern.slf4j.Slf4j;
+import net.logstash.logback.argument.StructuredArguments;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -27,16 +28,24 @@ public class MyController {
 
     @GetMapping("/profile/{userName}")
     public Profile getProfileDetails(@PathVariable String userName) {
+        long startTime = System.currentTimeMillis();
         log.info("Started Get Profile Details API Call with Username {}", userName);
 
         Profile profile = profileService.getUserProfile(userName);
 
         log.info("Ended Get Profile Details API Call with Username {}", userName);
+        log.info("Request Log",
+                StructuredArguments.kv("action","GET-PROFILE"),
+                StructuredArguments.kv("user",userName),
+                StructuredArguments.kv("responseTime",(System.currentTimeMillis()-startTime)+"ms"),
+                StructuredArguments.kv("httpStatus",HttpStatus.OK),
+                StructuredArguments.kv("httpStatus",HttpStatus.OK.value()));
         return profile;
     }
 
     @PostMapping("/subscriber")
     public ResponseEntity<Profile> createSubscriber(@RequestBody String userName) {
+        long startTime = System.currentTimeMillis();
         log.info("Started Create New Subscriber API Call with Username {}", userName);
 
         String url = profileUrl + "/" + userName;
@@ -44,8 +53,13 @@ public class MyController {
 
         ResponseEntity<Profile> subscriberResponseEntity = new ResponseEntity(profileResponseEntity.getBody(), HttpStatus.CREATED);
 
-
         log.info("Started Create New Subscriber API Call with Username {}", profileResponseEntity.getBody().getUserName());
+        log.info("Request Log",
+                StructuredArguments.kv("action","CREATE-SUBSCRIBER"),
+                StructuredArguments.kv("user",userName),
+                StructuredArguments.kv("responseTime",(System.currentTimeMillis()-startTime)+"ms"),
+                StructuredArguments.kv("httpStatus",HttpStatus.CREATED),
+                StructuredArguments.kv("httpStatusCode",HttpStatus.CREATED.value()));
         return subscriberResponseEntity;
     }
 }
